27314f2bdead9252a5c4361f159dcfdb13137be2
muskaracabey <muskaracabey@hepsiburada.com>
2024-09-30 17:58:16 +0300

    added products controller

---
+++ Muskartech.QRMenu.API/Controllers/V1/ProductsController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Muskartech.QRMenu.API.Controllers.V1;
+
+[Route("api/v1/products")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+}
